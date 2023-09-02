347353bf0dfaaf8f56b1116ea63ccdd088c315bd
fatemeh paghar <f.paghar@gmail.com>
2023-09-03 00:15:32 +0330

    Add: App folder - app-wide settings, styles and providers

---
+++ src/app/index.tsx
@@ -0,0 +1,22 @@
+import { HashRouter } from "react-router-dom";
+
+// core styles
+import "./scss/volt.scss";
+
+// vendor styles
+import "react-datetime/css/react-datetime.css";
+
+import HomePage from "../pages/HomePage";
+import ScrollToTop from "../components/ScrollToTop";
+
+
+const App = () => {
+    return (
+        <HashRouter>
+            <ScrollToTop />
+            <HomePage />
+        </HashRouter>
+    );
+};
+
+export default App;
